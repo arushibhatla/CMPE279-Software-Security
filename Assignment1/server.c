9eb8246bbd912b22067e89cc87bc7987ab608906
Shubham Singh <shubham.zingh@gmail.com>
2021-04-14 21:17:12 -0700

    Update server.c

--- Assignment1/server.c
+++ Assignment1/server.c
@@ -94,6 +94,7 @@ int main(int argc, char const *argv[])
         printf("%s\n",buffer );
         send(new_socket , hello , strlen(hello) , 0 );
         printf("Hello message sent\n");
+        printf("inside child process, uid: %d", getuid());
     }
     else // parent waits for the child to exit 
     {
